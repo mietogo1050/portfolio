08ad18585b53be783c945d0ede98173698d4428d
Mie Togo <mie.togo55@gmail.com>
2026-07-27 13:14:44 +0900

    works_07を追加

--- js/common.js
+++ js/common.js
@@ -53,6 +53,14 @@ Vue.component('item-card', {
             return Array.isArray(cat) ? cat.join(', ') : cat;
         },
     },
+    computer:{
+        sortedItemList() {
+            return [...this.itemList].sort((a, b) => {
+                        // 日付を比較して新しい順（降順）にする
+                        return new Date(b.date) - new Date(a.date);
+                    });
+    },
+    },
     template: `
     <div class="works-list">
     <a href="#" class="works-item" v-for="item in items" :key="item.id" @click.prevent="openModal(item)">
